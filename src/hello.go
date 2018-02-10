3af7cc64df01e36ff6a5d88ac338bf158837c7c8
Cheng-Yi Chiang <cychiang@chromium.org>
2018-02-10 21:24:21 +0800

    Add route to submit a match result
    use /add to submit a match result

--- src/hello.go
+++ src/hello.go
@@ -1,6 +1,7 @@
 package guestbook
 
 import (
+        "fmt"
         "html/template"
         "net/http"
         "time"
@@ -18,15 +19,78 @@ type Greeting struct {
 }
 // [END greeting_struct]
 
+// [START match_struct]
+type Match struct {
+        Tournament string
+        Submitter  string
+        Winner     string
+        Loser      string
+        Date       time.Time
+}
+// [END match_struct]
+
 func init() {
         http.HandleFunc("/", root)
         http.HandleFunc("/sign", sign)
+        http.HandleFunc("/add", add_match_result)
+        http.HandleFunc("/submit_match_result", submit_match_result)
 }
 
 // guestbookKey returns the key used for all guestbook entries.
 func guestbookKey(c appengine.Context) *datastore.Key {
         // The string "default_guestbook" here could be varied to have multiple guestbooks.
         return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+}
+
+
+const addMatchForm = `
+<html>
+  <head>
+    <title>Add a match result</title>
+  </head>
+  <body>
+    <form action="/submit_match_result" method="post">
+      <div><p>winner</p><textarea name="winner" rows="3" cols="60"></textarea></div>
+      <div><p>loser</p><textarea name="loser" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Add a match result"></div>
+    </form>
+  </body>
+</html>
+`
+
+// [START add_match_result]
+func add_match_result(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, addMatchForm)
+}
+
+// [START submit_match_result]
+func submit_match_result(w http.ResponseWriter, r *http.Request) {
+        // [START new_context]
+        c := appengine.NewContext(r)
+        // [END new_context]
+        g := Match{
+                Tournament: "Default",
+                Winner: r.FormValue("winner"),
+                Loser: r.FormValue("loser"),
+                Date:    time.Now(),
+        }
+        // [START if_user]
+        if u := user.Current(c); u != nil {
+                g.Submitter= u.String()
+        }
+        // We set the same parent key on every Greeting entity to ensure each Greeting
+        // is in the same entity group. Queries across the single entity group
+        // will be consistent. However, the write rate to a single entity group
+        // should be limited to ~1/second.
+        key := datastore.NewIncompleteKey(c, "Match", guestbookKey(c))
+        _, err := datastore.Put(c, key, &g)
+        if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
+        http.Redirect(w, r, "/", http.StatusFound)
+        // [END if_user]
+
 }
 
 // [START func_root]
