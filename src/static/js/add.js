7964949dbeed52d6a753c6132e5b23d84c47dff5
Cheng-Yi Chiang <cychiang@chromium.org>
2018-02-12 01:37:00 +0800

    Query JSON API from /user to get user names
    After getting user names, create Vue objects and set those user names
    as options.

--- src/static/js/add.js
+++ src/static/js/add.js
@@ -1,33 +1,48 @@
 Vue.component('v-select', VueSelect.VueSelect);
 
-new Vue({
-  el: '#winner_select',
-  data: function() {
-      return {
-        users: [
-            {"Name": "moja"},
-            {"Name": "jimmy"},
-        ],
-        selected: {"Name": " "}
-     }
-  },
+function createVueElements(r) {
+  users = JSON.parse(r);
+  console.log("users = " + users);
+  var v_w = new Vue({
+    el: '#winner_select',
+    data: function() {
+        return {
+          users: users,
+          selected: {"Name": " "}
+       }
+    },
 
-  methods: {
+    methods: {
+    }
+  });
+
+  var v_l = new Vue({
+    el: '#loser_select',
+    data: function() {
+        return {
+          users: users,
+          selected: {"Name": " "}
+       }
+    },
+
+    methods: {
+    }
+  });
+}
+
+function httpGetAsync(theUrl, callback)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          callback(xmlHttp.responseText);
   }
-});
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
+}
 
-new Vue({
-  el: '#loser_select',
-  data: function() {
-      return {
-        users: [
-            {"Name": "moja"},
-            {"Name": "jimmy"},
-        ],
-        selected: {"Name": " "}
-     }
-  },
-
-  methods: {
-  }
-});
+function getUsers () {
+  console.log("get users");
+  // Get available user data from JSON API.
+  httpGetAsync(location.origin + "/users", createVueElements);
+}
