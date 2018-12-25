571c8f10f0a4cb149077b022d1323fd8060fd059
cookieben <cookieben@gmail.com>
2018-12-25 04:37:17 -0800

    Fork codebase from https://github.com/mafredri/go-trueskill

---
+++ src/trueskill/draw.go
@@ -0,0 +1,19 @@
+package trueskill
+
+import (
+	"math"
+
+	"github.com/mafredri/go-trueskill/gaussian"
+)
+
+func drawProbability(beta, drawMargin, totalPlayers float64) float64 {
+	return 2*gaussian.NormCdf(drawMargin/(math.Sqrt(totalPlayers)*beta)) - 1
+}
+
+func drawMargin(beta, drawProb, totalPlayers float64) float64 {
+	// totalPlayers represents the total number of players for both team A
+	// and team B. Considering a match between 3 teams, A, B and C, we would
+	// call drawMargin two times. One time with len(A) + len(B), the other
+	// time with len(B) + len(C).
+	return -math.Sqrt((totalPlayers)*beta*beta) * gaussian.NormPpf((1-drawProb)/2)
+}
---
+++ src/trueskill/player.go
@@ -0,0 +1,34 @@
+package trueskill
+
+import (
+	"fmt"
+
+	"github.com/mafredri/go-trueskill/gaussian"
+)
+
+// Player is a player with a certain skill (mu, sigma).
+type Player struct {
+	gaussian.Gaussian
+}
+
+// NewPlayer returns a player from the provided mu (mean) and sigma
+// (standard deviation).
+func NewPlayer(mu, sigma float64) Player {
+	return Player{
+		Gaussian: gaussian.NewFromMeanAndStdDev(mu, sigma),
+	}
+}
+
+// Mu returns the player mean.
+func (p Player) Mu() float64 {
+	return p.Mean()
+}
+
+// Sigma returns the player standard deviation.
+func (p Player) Sigma() float64 {
+	return p.StdDev()
+}
+
+func (p Player) String() string {
+	return fmt.Sprintf("Player(mu=%.3f sigma=%.3f)", p.Mu(), p.Sigma())
+}
